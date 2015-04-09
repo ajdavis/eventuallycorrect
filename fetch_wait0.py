467465f93176632b79bece3e8af2dc3a6da49a4c
A. Jesse Jiryu Davis <jesse@mongodb.com>
2015-04-09 09:09:39 -0400

    Delete HTTP tests.

--- fetch_wait0.py
+++
@@ -1,8 +0,0 @@
-import unittest
-import requests
-
-
-class MyTestCase(unittest.TestCase):
-    def test_http_fetch(self):
-        response = requests.get("http://www.tornadoweb.org")
-        self.assertEqual(200, response.status_code)
