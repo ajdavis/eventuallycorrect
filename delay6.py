5885832da473b5cb4fb46c77d4dd8461f8bba572
A. Jesse Jiryu Davis <jesse@mongodb.com>
2015-04-09 17:08:24 -0400

    delay6 should pass.

--- delay6.py
+++ delay6.py
@@ -8,7 +8,7 @@ from my_application import delay_async
 class MyTestCase(testing.AsyncTestCase):
     def test_delay(self):
         start = time.time()
-        delay_async(2, callback=self.stop)
+        delay_async(1, callback=self.stop)
         self.wait()
         duration = time.time() - start
         self.assertAlmostEqual(duration, 1, places=2)
